6875abde1f5112f583e675612b9385ac6987d1c7
Cainã Costa <me@cfcosta.com>
2024-10-31 14:53:06 -0300

    chore(nix): add package dependencies

--- flake.nix
+++ flake.nix
@@ -89,7 +89,19 @@
           muchat = rustPlatform.buildRustPackage {
             name = "muchat";
             src = ./.;
-            cargoLock.lockFile = ./Cargo.lock;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+              outputHashes = {
+                "blade-graphics-0.5.0" = "sha256-j/JI34ZPD7RAHNHu3krgDLnIq4QmmZaZaU1FwD7f2FM";
+                "collections-0.1.0" = "sha256-D/arD3BtyHanCltA7fs0rfffADzZmJG+UiPsi3q+ckE=";
+                "cosmic-text-0.11.2" = "sha256-TLPDnqixuW+aPAhiBhSvuZIa69vgV3xLcw32OlkdCcM";
+                "font-kit-0.14.1" = "sha256-qUKvmi+RDoyhMrZ7T6SoVAyMc/aasQ9Y/okzre4SzXo=";
+                "xim-0.4.0" = "sha256-BXyaIBoqMNbzaSJqMadmofdjtlEVSoU6iogF66YP6a4=";
+                "xkbcommon-0.7.0" = "sha256-2RjZWiAaz8apYTrZ82qqH4Gv20WyCtPT+ldOzm0GWMo=";
+              };
+            };
+
             doCheck = false;
           };
         } // scripts;
