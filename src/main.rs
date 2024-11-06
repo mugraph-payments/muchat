0c346947a602578c43d958a85f49408cb5b71e9f
Miguel Tavares <migueloliveiradev@gmail.com>
2024-11-06 16:02:20 -0300

    feat: chat UI

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,9 @@
-use gpui::*;
+use home::Application;
 
-struct HelloWorld {
-    text: SharedString,
-}
+mod home;
 
-impl Render for HelloWorld {
-    fn render(&mut self, _cx: &mut ViewContext<Self>) -> impl IntoElement {
-        div()
-            .flex()
-            .bg(rgb(0x1e1e2e))
-            .size_full()
-            .justify_center()
-            .items_center()
-            .text_xl()
-            .text_color(rgb(0xcdd6f4))
-            .child(format!("Hello, {}!", &self.text))
-    }
-}
-
-fn main() {
-    App::new().run(|cx: &mut AppContext| {
-        cx.open_window(WindowOptions::default(), |cx| {
-            cx.new_view(|_cx| HelloWorld {
-                text: "World".into(),
-            })
-        })
-        .unwrap();
-    });
+pub fn main() -> iced::Result {
+    iced::application("muchat", Application::update, Application::view)
+        .theme(Application::theme)
+        .run()
 }
