0e3f62a01a17b873f5dbadb9a8b3165a8d1bdd06
Danilo Arantes <dfilho@purdue.edu>
2024-11-13 16:02:00 -0300

    feat: Adds command and response types from simplex typescript client

--- tauri-chat/src/App.tsx
+++ tauri-chat/src/App.tsx
@@ -7,12 +7,36 @@ function App() {
   return (
     <main className={classes.container}>
       <div>
-        <div className={`${classes.status} ${client.isConnected ? classes.connected : classes.disconnected }`}>{client.isConnected ? 'Connected' : 'Disconnected'}</div>
+        <div
+          className={`${classes.status} ${
+            client.isConnected ? classes.connected : classes.disconnected
+          }`}
+        >
+          {client.isConnected ? 'Connected' : 'Disconnected'}
+        </div>
       </div>
 
       <div id="messages" className={classes.chatBody}>
         {client.messages.map((msg, index) => {
-          return <div className={classes.chatItem} key={index}>{JSON.stringify(msg)}</div>;
+          return (
+            <div className={classes.chatItem} key={index}>
+              <span style={{ display: 'block' }}>{msg.resp.type}</span>
+              {msg.resp.type === 'newChatItems' && (
+                <span>
+                  {msg.resp.chatItems.map((item) =>
+                    item.chatItem.content.type === 'rcvMsgContent' ? (
+                      <div>{item.chatItem.content.msgContent.text}</div>
+                    ) : (
+                      <div>Other message type</div>
+                    ),
+                  )}
+                </span>
+              )}
+              {msg.resp.type !== 'newChatItems' && (
+                <span>{JSON.stringify(msg)}</span>
+              )}
+            </div>
+          );
         })}
       </div>
     </main>
--- tauri-chat/src/useWebSocket.ts
+++ tauri-chat/src/useWebSocket.ts
@@ -1,18 +1,18 @@
 import WebSocket, { Message } from '@tauri-apps/plugin-websocket';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { ChatResponse } from './lib/response';
 // when using `"withGlobalTauri": true`, you may use
 // const WebSocket = window.__TAURI__.websocket;
 
-type CommandPayload = {
-  corrId?: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  resp: any;
+export type ServerResponse = {
+  cmd: string;
+  resp: ChatResponse;
 }
 
 export function useWebSocket() {
   const [isConnected, setIsConnected] = useState(false);
   const webSocketClient = useRef<WebSocket | null>(null);
-  const [messages, setMessages] = useState<CommandPayload[]>([]);
+  const [messages, setMessages] = useState<ServerResponse[]>([]);
   const firstRun = useRef(true);
 
   const disconnect = useCallback(async () => {
