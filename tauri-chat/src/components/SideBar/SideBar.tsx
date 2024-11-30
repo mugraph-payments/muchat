54e8677cf62e0768790166b53e2fe8faf613df31
Miguel Tavares <migueloliveiradev@gmail.com>
2024-11-30 10:08:06 -0300

    feat: add separator component

--- tauri-chat/src/components/SideBar/SideBar.tsx
+++ tauri-chat/src/components/SideBar/SideBar.tsx
@@ -1,14 +1,17 @@
 import useChatContext from "@/useChatContext";
 import ContactListv2 from "@/components/ContactList/ContactListV2";
+import { Separator } from "@/components/Separator";
 
 function SideBar() {
   const { contacts } = useChatContext();
 
   return (
-    <div className="bg-theme-mantle flex flex-col items-center p-5 gap-2 border-r-[1px] border-theme-base">
-      <h1 className="text-2xl">
+    <div className="bg-theme-mantle flex flex-col p-4 gap-2 border-r-[1px] border-theme-base">
+      <h1 className="text-2xl text-left">
         Chats ({contacts.size})
       </h1>
+
+      <Separator />
 
       <div>
         <ContactListv2 />
