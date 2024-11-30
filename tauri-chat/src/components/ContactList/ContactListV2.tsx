6d0d3ce7ae933f4fd12731cbbe65e8496de4f833
Miguel Tavares <migueloliveiradev@gmail.com>
2024-11-30 09:01:20 -0300

    feat: add contacts in sidebar

--- tauri-chat/src/components/ContactList/ContactListV2.tsx
+++ tauri-chat/src/components/ContactList/ContactListV2.tsx
@@ -2,14 +2,16 @@ import useChatContext from "../../useChatContext";
 import Button from "../Button/Button";
 
 function ContactListv2() {
-  const { contacts } = useChatContext();
+  const { contacts, setSelectedChatId, selectedChatId } = useChatContext();
 
   return (
     <div className="flex flex-col gap-2">
-      {[...contacts].map(([_, contact], index) => {
+      {[...contacts].map(([cId, contact], index) => {
         return (
           <Button
             key={index}
+            className={selectedChatId == cId ? "bg-theme-text text-background" : "bg-muted text-muted-foreground"}
+            onClick={() => setSelectedChatId(cId)}
           >
             {contact.localDisplayName}
           </Button>
--- tauri-chat/src/components/SideBar/SideBar.tsx
+++ tauri-chat/src/components/SideBar/SideBar.tsx
@@ -1,9 +1,14 @@
+import useChatContext from "../../useChatContext";
 import ContactListv2 from "../ContactList/ContactListV2";
 
 function SideBar() {
+  const { contacts } = useChatContext();
+
   return (
-    <div className="bg-theme-mantle flex flex-col items-center p-5 gap-2">
-      <h1 className="text-2xl">Chats</h1>
+    <div className="bg-theme-mantle flex flex-col items-center p-5 gap-2 border-r-[1px] border-theme-base">
+      <h1 className="text-2xl">
+        Chats ({contacts.size})
+      </h1>
 
       <div>
         <ContactListv2 />
