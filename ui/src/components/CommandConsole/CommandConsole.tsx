2f5ed382ccf3978fc5d40e2d1f3090cada0839a0
Danilo Arantes <dfilho@purdue.edu>
2024-12-06 02:09:14 -0300

    chore: Change relative paths to absolute alias

--- ui/src/components/CommandConsole/CommandConsole.tsx
+++ ui/src/components/CommandConsole/CommandConsole.tsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { ServerResponse } from "@/lib/response";
 import { ChatCommandMessage } from "@/lib/command";
 import useChatContext from "@/useChatContext";
-import MessageInput from "../MessageInput/MessageInput";
+import MessageInput from "@/components/MessageInput/MessageInput";
 
 type CommandConsoleProps = {
   client: ReturnType<typeof useWebSocket>;
