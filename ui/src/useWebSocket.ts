77c1fad86b06c1ef2235bdea611ede941ceae577
Danilo Arantes F. <dfilho@purdue.edu>
2024-12-09 20:30:45 -0300

    feat: Adds tauri shell plugin and spawns simplex-chat child process

--- ui/src/useWebSocket.ts
+++ ui/src/useWebSocket.ts
@@ -7,10 +7,35 @@ import {
 } from "./lib/response";
 import useChatContext from "./useChatContext";
 import { ChatClient } from "./lib/client";
+import { Child, Command } from "@tauri-apps/plugin-shell";
+
 // when using `"withGlobalTauri": true`, you may use
 // const WebSocket = window.__TAURI__.websocket;
 
+export async function spawnSimplexServer() {
+  const command = Command.create("simplex-chat", [
+    "-p",
+    "5225",
+    "-d",
+    "~/muchat-server.db",
+  ]);
+  command.on("close", (data) => {
+    console.log(
+      `command finished with code ${data.code} and signal ${data.signal}`,
+    );
+  });
+  command.on("error", (error) => console.error(`command error: "${error}"`));
+  command.stdout.on("data", (line) => console.log(`command stdout: "${line}"`));
+  command.stderr.on("data", (line) => console.log(`command stderr: "${line}"`));
+
+  const child = await command.spawn();
+  console.log(`Spawned process with PID ${child.pid}`);
+
+  return child;
+}
+
 export function useWebSocket() {
+  const simplexProcess = useRef<Child | null>(null);
   const webSocketClient = useRef<ChatClient | null>(null);
   const firstRun = useRef(true);
   const {
@@ -73,13 +98,21 @@ export function useWebSocket() {
     if (!firstRun.current) return;
     firstRun.current = false;
 
-    console.log("🟩 Connecting...");
     async function connect() {
+      console.log("🟩 Connecting...");
       webSocketClient.current = await ChatClient.create();
       setIsConnected(true);
       initChatClient();
     }
-    connect();
+
+    if (!simplexProcess.current) {
+      spawnSimplexServer().then((child) => {
+        simplexProcess.current = child;
+        setTimeout(() => connect(), 500);
+      });
+    } else {
+      connect();
+    }
 
     return () => {
       console.log("🟥 Disconnecting");
