58b2b41c6dbad8b139d3a31ae01cb44533185a42
Danilo Arantes F. <dfilho@purdue.edu>
2024-12-14 18:13:09 -0300

    feat: Transforms CreateUserDialog into UserSettings component + Adds user settings components with option to set active, create and remove users + Adds components subfolders

---
+++ ui/src/components/forms/index.tsx
@@ -0,0 +1,3 @@
+export type FormProps<T> = {
+  onSubmit?: (data: T) => void;
+};
--- ui/src/lib/utils.ts
+++ ui/src/lib/utils.ts
@@ -1,5 +1,15 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { CRChatCmdError } from "./response";
+
+export const parseSimplexWsChatError = ({ chatError }: CRChatCmdError) => {
+  switch (chatError.type) {
+    case "error":
+      return chatError.errorType.type;
+    default:
+      return chatError.type;
+  }
+};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
