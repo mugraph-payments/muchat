3f4de7f52a7fa105258d4a96495628ad7a69f3c3
Danilo Arantes F. <dfilho@purdue.edu>
2024-12-14 14:24:22 -0300

    feat: Improves command console for faster render and responsiveness + Avoid full re-renders of command console by changing the visibility toggle from display to opacity + Moves consoleMessages render to useMemo hook

--- ui/src/components/CommandConsole/CommandConsole.tsx
+++ ui/src/components/CommandConsole/CommandConsole.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { ServerResponse } from "@/lib/response";
 import { ChatCommandMessage } from "@/lib/command";
 import useChatContext from "@/useChatContext";
@@ -39,26 +39,43 @@ const CommandConsole = () => {
   const handleCommandSubmit = async (input: string) => {
     if (input.trim()) {
       const corrId = (await client.current?.sendChatCommandStr(input)) ?? "";
-      setConsoleMessages((prev) => [...prev, { corrId, cmd: input }]);
+      setConsoleMessages((prev) => {
+        prev.push({ corrId, cmd: input });
+        return prev;
+      });
     }
   };
 
+  const Messages = useMemo(
+    () => (
+      <>
+        {consoleMessages.map((cmd, index) => (
+          <div
+            key={`${index}-${cmd.corrId}`}
+            className="text-sm w-full break-all"
+          >
+            {typeof (cmd as ChatCommandMessage).cmd === "string"
+              ? `> ${(cmd as ChatCommandMessage).cmd}`
+              : JSON.stringify(cmd)}
+          </div>
+        ))}
+      </>
+    ),
+    [consoleMessages],
+  );
+
   return (
     <div
-      className={`space-y-4 mb-4 absolute top-0 left-0 w-full bg-background p-4 ${isActive ? "block" : "hidden"} shadow-md`}
+      className={`space-y-4 mb-4 absolute top-0 left-0 w-full bg-background p-4 ${
+        isActive ? "opacity-100 visible" : "opacity-0 invisible"
+      } transition-opacity duration-150 shadow-md`}
     >
       <h3>Console</h3>
       <div
         ref={consoleBoxRef}
         className="min-h-52 max-h-64 overflow-y-auto overflow-x-hidden bg-gray-800 p-2 rounded"
       >
-        {consoleMessages.map((cmd, index) => (
-          <div key={index} className="text-sm w-full break-all">
-            {typeof (cmd as ChatCommandMessage).cmd === "string"
-              ? `> ${(cmd as ChatCommandMessage).cmd}`
-              : JSON.stringify(cmd)}
-          </div>
-        ))}
+        {Messages}
       </div>
       <MessageInput onSubmit={handleCommandSubmit} />
     </div>
