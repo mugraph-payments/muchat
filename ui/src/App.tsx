a0b782735a08e0a849923d5a29cb72b7cd0fce17
Miguel Tavares <migueloliveiradev@gmail.com>
2024-12-18 15:19:00 -0300

    chore: add toaster component to react tree

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,12 +1,16 @@
 import Chat from "@/Chat";
 import SideBar from "@/components/SideBar/SideBar";
+import { Toaster } from "@/components/Sonner";
 
 function App() {
   return (
-    <main className="flex">
-      <SideBar />
-      <Chat />
-    </main>
+    <>
+      <main className="flex">
+        <SideBar />
+        <Chat />
+      </main>
+      <Toaster />
+    </>
   );
 }
 
