5435544bea7de85088ac0da49e6e046f46610445
Danilo Arantes F. <dfilho@purdue.edu>
2024-12-20 13:21:14 -0300

    feat: Include SidebarTrigger into Chat header and improve layout

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,17 +1,18 @@
 import Chat from "@/components/features/chat/Chat";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/Sidebar";
+import { SidebarProvider } from "@/components/ui/Sidebar";
 import { Toaster } from "@/components/ui/Sonner";
 import { AppSidebar } from "./components/features/AppSidebar";
 
 function App() {
   return (
     <SidebarProvider>
-      <main className="flex w-full">
-        <AppSidebar />
-        <SidebarTrigger />
-        <Chat />
-      </main>
-      <Toaster />
+      <div className="h-screen w-full flex flex-col">
+        <main className="flex flex-1 w-full overflow-hidden">
+          <AppSidebar />
+          <Chat />
+        </main>
+        <Toaster />
+      </div>
     </SidebarProvider>
   );
 }
