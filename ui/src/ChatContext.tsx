43753b05bf5811e53b41789061091bb7372ce568
Danilo Arantes F. <dfilho@purdue.edu>
2025-01-07 17:31:19 -0300

    feat: Implements sending messages to groups

--- ui/src/ChatContext.tsx
+++ ui/src/ChatContext.tsx
@@ -9,6 +9,7 @@ import {
   UserInfo,
   ChatInfoType,
   Group,
+  GroupInfo,
 } from "@/lib/response";
 import { useSimplexCli } from "./useSimplexCli";
 import { ChatType } from "./lib/command";
@@ -25,10 +26,10 @@ export interface ChatContextType {
   contacts: Map<number, Contact>;
   setContacts: (contacts: Map<number, Contact>) => void;
   setContact: (c: Contact) => void;
-  directChats: Map<number, ChatItem[]>;
+  directChats: Map<string, ChatItem[]>;
   setDirectChats: (chats: AChatItem[]) => void;
-  selectedChatId: number;
-  setSelectedChatId: (id: number) => void;
+  selectedChatId: string;
+  setSelectedChatId: (id: string) => void;
   activeUser: User | null;
   setActiveUser: (user: User | null) => void;
   contactLink: UserContactLink | null;
@@ -37,6 +38,20 @@ export interface ChatContextType {
   setGroups: (groups: Map<number, Group>) => void;
 }
 
+export const getChatKey = ({
+  contact,
+  group,
+}: {
+  contact?: Contact;
+  group?: GroupInfo;
+}) => {
+  return contact
+    ? `${ChatType.Direct}${contact.contactId}`
+    : group
+      ? `${ChatType.Group}${group.groupId}`
+      : "";
+};
+
 const ChatContext = createContext<ChatContextType | undefined>(undefined);
 
 const ChatProvider = ({ children }: { children: ReactNode }) => {
@@ -44,10 +59,11 @@ const ChatProvider = ({ children }: { children: ReactNode }) => {
   const [messages, setMessages] = useState<ServerResponse[]>([]);
   const [contacts, setContacts] = useState<Map<number, Contact>>(new Map([]));
   const [groups, setGroups] = useState<Map<number, Group>>(new Map());
-  const [directChats, setDirectChats] = useState<Map<number, ChatItem[]>>(
+  const [directChats, setDirectChats] = useState<Map<string, ChatItem[]>>(
     new Map(),
   );
-  const [selectedChatId, setSelectedChatId] = useState(-1);
+  const [selectedChatId, setSelectedChatId] = useState("");
+
   const [activeUser, setActiveUser] = useState<null | User>(null);
   const [contactLink, setContactLink] = useState<UserContactLink | null>(null);
   const [users, setUsers] = useState<UserInfo[]>([]);
@@ -55,6 +71,7 @@ const ChatProvider = ({ children }: { children: ReactNode }) => {
     (msg: ServerResponse) => setMessages((msgs) => [...msgs, msg]),
     [],
   );
+
   const updateDirectChats = useCallback(
     (chats: AChatItem[]) => {
       setDirectChats((curChats) => {
@@ -63,6 +80,7 @@ const ChatProvider = ({ children }: { children: ReactNode }) => {
           if (msg.chatInfo.type === ChatInfoType.Direct) {
             const contact = msg.chatInfo.contact;
 
+            // Append to contacts if not there
             if (!contacts.has(contact.contactId)) {
               setContacts((c) => {
                 const newContacts = new Map(c);
@@ -71,18 +89,32 @@ const ChatProvider = ({ children }: { children: ReactNode }) => {
               });
             }
 
-            const currentMessages =
-              updatedDirectChats.get(contact.contactId) ?? [];
-            updatedDirectChats.set(contact.contactId, [
-              ...currentMessages,
-              msg.chatItem,
-            ]);
+            const chatKey = getChatKey({ contact });
+            const currentMessages = updatedDirectChats.get(chatKey) ?? [];
+            updatedDirectChats.set(chatKey, [...currentMessages, msg.chatItem]);
+          }
+
+          if (msg.chatInfo.type === ChatInfoType.Group) {
+            const group = msg.chatInfo.groupInfo;
+
+            // Append to groups if not there
+            if (!groups.has(group.groupId)) {
+              setGroups((g) => {
+                const newGroups = new Map(g);
+                newGroups.set(group.groupId, { groupInfo: group, members: [] });
+                return newGroups;
+              });
+            }
+
+            const chatKey = getChatKey({ group });
+            const currentMessages = updatedDirectChats.get(chatKey) ?? [];
+            updatedDirectChats.set(chatKey, [...currentMessages, msg.chatItem]);
           }
         });
         return updatedDirectChats;
       });
     },
-    [contacts],
+    [contacts, groups],
   );
   const setContact = useCallback(
     (c: Contact) =>
@@ -103,15 +135,24 @@ const ChatProvider = ({ children }: { children: ReactNode }) => {
       }
       client.current?.apiListContacts(data.user.userId.toString());
       setActiveUser(data.user);
-      setSelectedChatId(-1);
+      setSelectedChatId("");
     },
     onChat: (data) => {
       switch (data.chat.chatInfo.type) {
         case ChatInfoType.Direct: {
           const newChatItems = data.chat.chatItems;
-          const contactId = data.chat.chatInfo.contact.contactId;
+          const contact = data.chat.chatInfo.contact;
           setDirectChats((chats) => {
-            chats.set(contactId, newChatItems);
+            chats.set(getChatKey({ contact }), newChatItems);
+            return chats;
+          });
+          break;
+        }
+        case ChatInfoType.Group: {
+          const newChatItems = data.chat.chatItems;
+          const group = data.chat.chatInfo.groupInfo;
+          setDirectChats((chats) => {
+            chats.set(getChatKey({ group }), newChatItems)
             return chats;
           });
           break;
@@ -134,6 +175,7 @@ const ChatProvider = ({ children }: { children: ReactNode }) => {
       const newGroups = new Map<number, Group>();
       groups.forEach(([groupInfo]) => {
         newGroups.set(groupInfo.groupId, { groupInfo, members: [] });
+        client.current?.apiGetChat(ChatType.Group, groupInfo.groupId);
       });
       setGroups(newGroups);
     },
